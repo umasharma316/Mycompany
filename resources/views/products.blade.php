8d711e30c401de5701692b47bcf9ce385e5b4b60
umasharma316 <umasharma316@gmail.com>
2018-01-17 16:49:15 +0530

    added new products

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -36,8 +36,8 @@
                             <div class="overlay">
                                 <div class="recent-work-inner">
                                     <h3><a href="#">Bubble Wrap</a></h3>
-                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
-                                    <a class="preview" href="images/portfolio/full/image1.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                    <p>There are different types of bubble wraps available with us with different guages.</p>
+                                    <a class="preview" href="images/portfolio/recent/image1.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                                 </div> 
                             </div>
                         </div>
@@ -49,8 +49,21 @@
                             <div class="overlay">
                                 <div class="recent-work-inner">
                                     <h3><a href="#">BOPP Tapes</a></h3>
-                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
-                                    <a class="preview" href="images/portfolio/full/image2.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                    <p>BOPP Tapes are available in all sizes at its best quality.</p>
+                                    <a class="preview" href="images/portfolio/recent/image2.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>          
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item joomla bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/image20.jpg" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Printed BOPP Tapes</a></h3>
+                                    <p>There is provision to customise the printing on BOPP Tapes with respect to different companies.</p>
+                                    <a class="preview" href="images/portfolio/recent/image20.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                                 </div> 
                             </div>
                         </div>          
@@ -63,24 +76,24 @@
                                 <div class="recent-work-inner">
                                     <h3><a href="#">Plastic Bags</a></h3>
                                     <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
-                                    <a class="preview" href="images/portfolio/full/image3.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                    <a class="preview" href="images/portfolio/recent/image3.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                                 </div> 
                             </div>
                         </div>        
                     </div><!--/.portfolio-item-->
 
-                    <div class="portfolio-item joomla wordpress apps col-xs-12 col-sm-4 col-md-3">
+                    <!-- <div class="portfolio-item joomla wordpress apps col-xs-12 col-sm-4 col-md-3">
                         <div class="recent-work-wrap">
                             <img class="img-responsive" src="images/portfolio/recent/image4.jpg" alt="">
                             <div class="overlay">
                                 <div class="recent-work-inner">
                                     <h3><a href="#">Design Your Bags</a></h3>
                                     <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
-                                    <a class="preview" href="images/portfolio/full/image4.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                    <a class="preview" href="images/portfolio/recent/image4.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                                 </div> 
                             </div>
                         </div>           
-                    </div><!--/.portfolio-item-->
+                    </div> --><!--/.portfolio-item-->
           
                     <div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
                         <div class="recent-work-wrap">
@@ -88,38 +101,38 @@
                             <div class="overlay">
                                 <div class="recent-work-inner">
                                     <h3><a href="#">Brown BOPP Tapes</a></h3>
-                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
-                                    <a class="preview" href="images/portfolio/full/image5.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                    <p>Brown BOPP Tapes in all sizes are available with us.</p>
+                                    <a class="preview" href="images/portfolio/recent/image5.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                                 </div> 
                             </div>
                         </div>      
                     </div><!--/.portfolio-item-->
 
-                    <div class="portfolio-item wordpress html apps col-xs-12 col-sm-4 col-md-3">
+                    <!-- <div class="portfolio-item wordpress html apps col-xs-12 col-sm-4 col-md-3">
                         <div class="recent-work-wrap">
                             <img class="img-responsive" src="images/portfolio/recent/image6.jpg" alt="">
                             <div class="overlay">
                                 <div class="recent-work-inner">
                                     <h3><a href="#">Paper Envelops</a></h3>
                                     <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
-                                    <a class="preview" href="images/portfolio/full/image6.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                    <a class="preview" href="images/portfolio/recent/image6.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                                 </div> 
                             </div>
                         </div>         
-                    </div><!--/.portfolio-item-->
+                    </div> --><!--/.portfolio-item-->
 
-                    <div class="portfolio-item wordpress html col-xs-12 col-sm-4 col-md-3">
+                    <!-- <div class="portfolio-item wordpress html col-xs-12 col-sm-4 col-md-3">
                         <div class="recent-work-wrap">
                             <img class="img-responsive" src="images/portfolio/recent/image9.jpg" alt="">
                             <div class="overlay">
                                 <div class="recent-work-inner">
                                     <h3><a href="#">Design Your Bags</a></h3>
                                     <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
-                                    <a class="preview" href="images/portfolio/full/image9.jpg"><i class="fa fa-eye"></i> View</a>
+                                    <a class="preview" href="images/portfolio/recent/image9.jpg"><i class="fa fa-eye"></i> View</a>
                                 </div> 
                             </div>
                         </div>          
-                    </div><!--/.portfolio-item-->
+                    </div> --><!--/.portfolio-item-->
 
              <div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
                         <div class="recent-work-wrap">
@@ -127,8 +140,152 @@
                             <div class="overlay">
                                 <div class="recent-work-inner">
                                     <h3><a href="#">Paper Tubes</a></h3>
-                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
-                                    <a class="preview" href="images/portfolio/full/image10.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                    <p>We deal with all kinds of paper tube required in textile packaging industry, in all sizes.</p>
+                                    <a class="preview" href="images/portfolio/recent/image10.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>      
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/image21.jpg" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Paper Tubes</a></h3>
+                                    <p>We deal with all kinds of paper tube required in textile packaging industry, in all sizes.</p>
+                                    <a class="preview" href="images/portfolio/recent/image21.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>      
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/image22.jpg" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Paper Tubes</a></h3>
+                                    <p>We deal with all kinds of paper tube required in textile packaging industry, in all sizes.</p>
+                                    <a class="preview" href="images/portfolio/recent/image22.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>      
+                    </div><!--/.portfolio-item-->
+
+
+                    <div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/image11.jpg" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">LD Bags</a></h3>
+                                    <p>There are many variations in guages of LD Bags and are provided in all sizes.</p>
+                                    <a class="preview" href="images/portfolio/recent/image11.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>      
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/image12.jpg" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Printed LD Bags</a></h3>
+                                    <p>We manufacture and Print the Polybags as required by the client.</p>
+                                    <a class="preview" href="images/portfolio/recent/image12.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>      
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/image13.jpg" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Bubble Wraps</a></h3>
+                                    <p>The bubble wraps are supplied in all types of guages and sizes.</p>
+                                    <a class="preview" href="images/portfolio/recent/image13.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>      
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/image14.jpg" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Gunny Bags</a></h3>
+                                    <p>We deal with supply of gunny bags with respect to their size and quantity.</p>
+                                    <a class="preview" href="images/portfolio/recent/image14.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>      
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/image15.jpg" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">BOPP Bags</a></h3>
+                                    <p>There are many variations in guages of LD Bags and are provided in all sizes.</p>
+                                    <a class="preview" href="images/portfolio/recent/image15.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>      
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/image16.jpg" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Suthli</a></h3>
+                                    <p>Suthli is supplied by us.</p>
+                                    <a class="preview" href="images/portfolio/recent/image16.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>      
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/image17.jpg" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Grey Boards</a></h3>
+                                    <p>we majorly deal with supply of grey boards across the country. It is made available in all standard sizes with/ without pasting.</p>
+                                    <a class="preview" href="images/portfolio/recent/image17.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>      
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/image18.jpg" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Grey Boards</a></h3>
+                                    <p>Grey boards using in packing are available in different gramage and sizes at best market price.</p>
+                                    <a class="preview" href="images/portfolio/recent/image18.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>      
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/image19.jpg" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Grey Boards</a></h3>
+                                    <p>Grey boards used in suitings and shirtings are supplied by us.</p>
+                                    <a class="preview" href="images/portfolio/recent/image19.jpg" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
                                 </div> 
                             </div>
                         </div>      
